8c3a9718a74bc848ed326900585ef14b138b6301
utsav <utsavporwal01@gmail.com>
2021-07-28 20:53:51 +0530

    ui changes

--- lib/utsav/shipping.dart
+++ lib/utsav/shipping.dart
@@ -144,11 +144,7 @@ class _ShippingState extends State<Shipping> {
                         Padding(
                           padding: const EdgeInsets.only(top: 20),
                           child: Text('ADD SHIPPING',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 35,
-                                fontWeight: FontWeight.bold
-                            ),),
+                            style: GoogleFonts.ptSans(color: Colors.white,fontSize: 35,fontWeight: FontWeight.bold),)
                         ),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -175,7 +171,8 @@ class _ShippingState extends State<Shipping> {
                               keyboardType:TextInputType.number,
                               decoration: InputDecoration(
                                 fillColor: Colors.white, filled: true,
-                                errorStyle: TextStyle(color: Color(0xFFFFD45F),fontWeight: FontWeight.bold,fontSize: 12),
+                                errorStyle:
+                                GoogleFonts.ptSans(color: Color(0xFFFFD45F),fontSize: 12,fontWeight: FontWeight.bold),
 
                                 prefix:  Text('\$'),
                                 hintText: 'Enter Shipping Cost here',
@@ -227,11 +224,7 @@ class _ShippingState extends State<Shipping> {
                             Padding(
                               padding: const EdgeInsets.only(top: 15,left: 50,bottom: 8),
                               child: Text('Select Driver',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20
-                                ),),
+                                style: GoogleFonts.ptSans(color: Colors.white,fontSize: 20,fontWeight: FontWeight.bold),)
                             ),
                           ],
                         ),
@@ -284,12 +277,10 @@ class _ShippingState extends State<Shipping> {
                               GestureDetector(
                                 child: Container(
                                   child:Center(child: Text('Ship',
-                                    style: TextStyle(fontWeight:FontWeight.bold,
+                                    style: GoogleFonts.ptSans(fontSize: 30,fontWeight: FontWeight.bold),
+                                  ),
+                                  ),
 
-                                        fontSize: 30
-                                    ),
-                                  ),
-                                  ),
                                   decoration: BoxDecoration(
                                     color:Color(0xFFFFD45F),
                                     borderRadius: BorderRadius.circular(45),
@@ -316,9 +307,7 @@ class _ShippingState extends State<Shipping> {
                               ),
                               GestureDetector(
                                 child: Container(
-                                  child:Center(child: Text('Cancel',style: TextStyle(fontWeight:FontWeight.bold,
-                                      fontSize: 30
-                                  ),
+                                  child:Center(child: Text('Cancel',style: GoogleFonts.ptSans(fontSize: 30,fontWeight: FontWeight.bold),
                                   ),
 
                                   ),
