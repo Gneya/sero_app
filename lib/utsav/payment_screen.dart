0f71b3047ed131449ec3754af46292a8ff5f1f4c
utsav <utsavporwal01@gmail.com>
2021-08-04 17:28:33 +0530

    resume half way done

--- lib/utsav/payment_screen.dart
+++ lib/utsav/payment_screen.dart
@@ -1265,25 +1265,27 @@ class _PaymentScreenState extends State<PaymentScreen> {
                               // print(list_of_m);
                             }
                           print(list_of_m);
-                          Map<String,dynamic> api= {
-                            "sells":[
-                              {
-                                "table_id" :shared.getInt("table_id")??0,
-                                "location_id": shared.getInt("bid")??1,
-                                "contact_id": double.parse(shared.getString("customer_id")??""),
-                                // "status": "draft",
-                                "is_suspend": 1,
-                                "products":list_of_m,
-                                "payments": [
-                                  {
-                                    "amount":cart.getTotalAmount(),
-                                  }
-                                ]
-                              }
-                            ]
-                          };
+
                           if(shared.getInt("order_id")==0)
                           {
+                            print('if called000000000000000000000000000000000000000000000000000000000000');
+                            Map<String,dynamic> api= {
+                              "sells":[
+                                {
+                                  "table_id" :shared.getInt("table_id")??0,
+                                  "location_id": shared.getInt("bid")??1,
+                                  "contact_id": double.parse(shared.getString("customer_id")??""),
+                                  // "status": "draft",
+                                  "is_suspend": 1,
+                                  "products":list_of_m,
+                                  "payments": [
+                                    {
+                                      "amount":cart.getTotalAmount(),
+                                    }
+                                  ]
+                                }
+                              ]
+                            };
                             var dio=Dio();
                             dio.options.headers["Authorization"]="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjMwYjE2MGVhNGUzMzA4ZTNiMjhhZGNlYWEwNjllZTA2NjI5Y2M4ZjMxMWFjZjUwMDFjZmZkMTE1ZDZlNTliZGI5NmJlZmQ3ZGYzYjRhNWNhIn0.eyJhdWQiOiIzIiwianRpIjoiMzBiMTYwZWE0ZTMzMDhlM2IyOGFkY2VhYTA2OWVlMDY2MjljYzhmMzExYWNmNTAwMWNmZmQxMTVkNmU1OWJkYjk2YmVmZDdkZjNiNGE1Y2EiLCJpYXQiOjE2MjU4OTY4MDcsIm5iZiI6MTYyNTg5NjgwNywiZXhwIjoxNjU3NDMyODA3LCJzdWIiOiI4Iiwic2NvcGVzIjpbXX0.OJ9XTCy8i5-f17ZPWNpqdT6QMsDgSZUsSY9KFEb-2O6HehbHt1lteJGlLfxJ2IkXF7e9ZZmydHzb587kqhBc_GP4hxj6PdVpoX_GE05H0MGOUHfH59YgSIQaU1cGORBIK2B4Y1j4wyAmo0O1i5WAMQndkKxA03UFGdipiobet64hAvCIEu5CipJM7XPWogo2gLUoWob9STnwYQuOgeTLKfMsMG4bOeaoVISy3ypALDJxZHi85Q9DZgO_zbBp9MMOvhYm9S1vPzoKCaGSx2zNtmOtCmHtUAxCZbu0TR2VDN7RpLdMKgPF8eLJglUhCur3BQnXZfYWlVWdG-T3PCKMvJvoE6rZcVXy2mVJUk3fWgldcOAhPRmQtUS563BR0hWQDJOL3RsRAjeesMhRouCtfmQBcW83bRindIiykYV1HrjdJBQNb3yuFFJqs9u7kgVFgZmwzsbd512t9Vfe1Cq_DhXbJM2GhIoFg72fKbGImu7UnYONUGB3taMmQn4qCXoMFnDl7glDLU9ib5pbd0matbhgkydHqThk5RZOPWje9W93j9RvwqwYL1OkcV9VXWcxYk0wwKRMqNtx74GLOUtIh8XJDK3LtDpRwLKer4dDPxcQHNgwkEH7iJt40bd9j27Mcyech-BZDCZHRSZbwhT7GnNeu2IluqVq3V0hCW3VsB8";
                             var r=await dio.post("https://pos.sero.app/connector/api/sell",data: json.encode(api));
@@ -1297,6 +1299,40 @@ class _PaymentScreenState extends State<PaymentScreen> {
                                 gravity: ToastGravity.BOTTOM,
                                 textColor: Colors.green,
                                 timeInSecForIosWeb: 4);
+                          }
+                          else{
+                            print('else0000000000000000000000000000000000000000000000000000000');
+                            Map<String,dynamic> api= {
+                              "products":list_of_m,
+                              "payments": [
+                                {
+                                  "amount":cart.getTotalAmount(),
+                                }
+                              ],
+                              "sells":[
+                                {
+
+                                  "is_suspend": 0,
+
+                                }
+                              ]
+                            };
+                            var dio=Dio();
+                            var vid = shared.getInt("order_id");
+                            dio.options.headers["Authorization"]="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjMwYjE2MGVhNGUzMzA4ZTNiMjhhZGNlYWEwNjllZTA2NjI5Y2M4ZjMxMWFjZjUwMDFjZmZkMTE1ZDZlNTliZGI5NmJlZmQ3ZGYzYjRhNWNhIn0.eyJhdWQiOiIzIiwianRpIjoiMzBiMTYwZWE0ZTMzMDhlM2IyOGFkY2VhYTA2OWVlMDY2MjljYzhmMzExYWNmNTAwMWNmZmQxMTVkNmU1OWJkYjk2YmVmZDdkZjNiNGE1Y2EiLCJpYXQiOjE2MjU4OTY4MDcsIm5iZiI6MTYyNTg5NjgwNywiZXhwIjoxNjU3NDMyODA3LCJzdWIiOiI4Iiwic2NvcGVzIjpbXX0.OJ9XTCy8i5-f17ZPWNpqdT6QMsDgSZUsSY9KFEb-2O6HehbHt1lteJGlLfxJ2IkXF7e9ZZmydHzb587kqhBc_GP4hxj6PdVpoX_GE05H0MGOUHfH59YgSIQaU1cGORBIK2B4Y1j4wyAmo0O1i5WAMQndkKxA03UFGdipiobet64hAvCIEu5CipJM7XPWogo2gLUoWob9STnwYQuOgeTLKfMsMG4bOeaoVISy3ypALDJxZHi85Q9DZgO_zbBp9MMOvhYm9S1vPzoKCaGSx2zNtmOtCmHtUAxCZbu0TR2VDN7RpLdMKgPF8eLJglUhCur3BQnXZfYWlVWdG-T3PCKMvJvoE6rZcVXy2mVJUk3fWgldcOAhPRmQtUS563BR0hWQDJOL3RsRAjeesMhRouCtfmQBcW83bRindIiykYV1HrjdJBQNb3yuFFJqs9u7kgVFgZmwzsbd512t9Vfe1Cq_DhXbJM2GhIoFg72fKbGImu7UnYONUGB3taMmQn4qCXoMFnDl7glDLU9ib5pbd0matbhgkydHqThk5RZOPWje9W93j9RvwqwYL1OkcV9VXWcxYk0wwKRMqNtx74GLOUtIh8XJDK3LtDpRwLKer4dDPxcQHNgwkEH7iJt40bd9j27Mcyech-BZDCZHRSZbwhT7GnNeu2IluqVq3V0hCW3VsB8";
+                            var r=await dio.put("https://pos.sero.app/connector/api/sell/$vid",data: json.encode(api));
+                            var v=r.data[0]["id"];
+                            print(v.toString());
+                            shared.setInt("order_id", v);
+
+                            Fluttertoast.showToast(
+                                msg: "Order on hold and Your Order Id is $v",
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.BOTTOM,
+                                textColor: Colors.green,
+                                timeInSecForIosWeb: 4);
+
+
                           }
                           cart.deleteAllCart();
 
@@ -1357,8 +1393,8 @@ class _PaymentScreenState extends State<PaymentScreen> {
                             {
 
                               Map<String,dynamic> product={
-                                "product_id":double.parse(cart.cartItem[index].productId),
-                                "variation_id":double.parse(variation![index]),
+                                "product_id":double.parse(cart.cartItem[index].productId.toString()),
+                                // "variation_id":double.parse(variation![index]),
                                 "quantity": cart.cartItem[index].quantity,
                                 "unit_price": cart.cartItem[index].unitPrice*cart.cartItem[index].quantity,
                               };
