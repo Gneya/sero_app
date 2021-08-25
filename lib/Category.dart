e054869081fbedc0e6b60ae65e7ee7fb7fdd8600
utsav <utsavporwal01@gmail.com>
2021-08-25 19:29:12 +0530

    issues resolved

--- lib/Category.dart
+++ lib/Category.dart
@@ -9,6 +9,7 @@ import 'package:flutter_nav_bar/selectable.dart';
 import 'package:flutter_nav_bar/utsav/notification.dart';
 import 'package:flutter_nav_bar/utsav/resume_screen.dart';
 import 'package:flutter_nav_bar/utsav/void.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -261,6 +262,8 @@ class _CategoryScreenState extends State<CategoryScreen> {
                     gravity: ToastGravity.BOTTOM,
                     textColor: Colors.green,
                     timeInSecForIosWeb: 10);
+                shared.setInt("seconds", 0);
+                Phoenix.rebirth(context);
               },
               onLongPress: () => print('THIRD CHILD LONG PRESS'),
             ),
